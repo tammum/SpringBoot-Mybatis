82b013fd4394216d6c34f1d091c7121fb774b111
tammum <tammumislam@gmail.com>
2018-07-03 11:45:04 +0600

    spring boot mybatis

---
+++ src/main/java/com/batisui/studentDatabase/StudentDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.batisui.studentDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDatabaseApplication.class, args);
+	}
+}
